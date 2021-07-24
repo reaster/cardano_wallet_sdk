168f822323bc1e612e7baecc5ce2602eeed08f51
Richard Easterling <richard@Richards-MacBook-Pro.local>
2021-07-24 11:32:03 -0700

    epoch and slot to timestamp functions

--- lib/src/util/ada_time.dart
+++ lib/src/util/ada_time.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'dart:math';
+
 ///
 /// Convert Cardano block time (secondsSinceEpoch in UTC) to DateTime and back.
 ///
@@ -24,3 +26,72 @@ class AdaDateTimeDecoder extends Converter<DateTime, int> {
 }
 
 final adaDateTime = AdaDateTime();
+
+//slots per second change: https://explorer.cardano.org/en/browse-epochs.html?perPage=50&page=5
+//1 slot = 20sec - epoch 207, slot 4492799, block 4490510, 2020/07/29 21:44:31 UTC
+//1 slot = 01sec - epoch 208, slot 4492800, block 4490511, 2020/07/29 21:44:51 UTC
+//1 slot per 20 seconds:
+final millisecondsPerSlot0_207 = 20 * 1000;
+final slotsPerEpoch0_207 = 21600;
+final firstOneSecondEpoch = 208;
+final millisecondsPerEpoch0_207 = slotsPerEpoch0_207 * millisecondsPerSlot0_207;
+// 1 slot per second
+final firstSlotOfEpoch208 = 4492800;
+final millisecondsPerSlotGt207 = 1 * 1000;
+final slotsPerEpochGt207 = 432000; //1 slot per second
+final millisecondsPerEpochGt207 = slotsPerEpochGt207 * millisecondsPerSlotGt207;
+//https://explorer.cardano.org/en/epoch.html?number=0&page=0perPage=10
+final epoch0Slot0Utc = DateTime(2017, 9, 23, 21, 44, 51).toUtc();
+final epoch0Slot0UtcMs = epoch0Slot0Utc.millisecondsSinceEpoch;
+
+///
+/// convert epoch to unix time in milliseconds
+///
+int epochToMilliseconds({int epoch = 0}) =>
+    min(firstOneSecondEpoch, epoch) * millisecondsPerEpoch0_207 +
+    max(0, epoch - firstOneSecondEpoch) * millisecondsPerEpochGt207 +
+    epoch0Slot0UtcMs;
+
+///
+/// convert epoch to UTC timestamp
+///
+DateTime epochToDateTime({int epoch = 0}) => DateTime.fromMillisecondsSinceEpoch(epochToMilliseconds(epoch: epoch), isUtc: true);
+
+///
+/// convert slot to unix time in milliseconds
+///
+int slotToMilliseconds({int slot = 0}) =>
+    min(firstSlotOfEpoch208, slot) * millisecondsPerSlot0_207 +
+    max(0, slot - firstSlotOfEpoch208) * millisecondsPerSlotGt207 +
+    epoch0Slot0UtcMs;
+
+///
+/// convert slot to UTC timestamp
+///
+DateTime slotToDateTime({int slot = 0}) => DateTime.fromMillisecondsSinceEpoch(slotToMilliseconds(slot: slot), isUtc: true);
+
+///
+/// Convert Cardano epoch (epoch * 432000 + 1506203091 in UTC) to DateTime and back.
+/// Unix timestamp (seconds since 1970-01-01)
+///
+// class EpochDateTime extends Codec<int, DateTime> {
+//   const EpochDateTime();
+//   final encoder = const EpochDateTimeEncoder();
+//   final decoder = const EpochDateTimeDecoder();
+//   DateTime encode(int epoch) => encoder.convert(epoch);
+//   int decode(DateTime dateTime) => decoder.convert(dateTime);
+// }
+
+// class EpochDateTimeEncoder extends Converter<int, DateTime> {
+//   const EpochDateTimeEncoder();
+//   @override
+//   DateTime convert(int epoch) => epochToDateTime(epoch: epoch);
+// }
+
+// class EpochDateTimeDecoder extends Converter<DateTime, int> {
+//   const EpochDateTimeDecoder();
+//   @override
+//   TODO int convert(DateTime dateTime) => (dateTime.millisecondsSinceEpoch / 432000).round() - 1506203091;
+// }
+
+// final epochDateTime = EpochDateTime();
--- test/ada_time_test.dart
+++ test/ada_time_test.dart
@@ -11,4 +11,54 @@ void main() {
       expect(now, equals(now2));
     });
   });
+  group('EpochDateTime -', () {
+    // test('codec', () {
+    //   final slot0DateTime = DateTime(2017, 9, 23, 21, 44, 51).toUtc();
+    //   final slot0EpochMsUtc = slot0DateTime.millisecondsSinceEpoch;
+    //   final now0 = DateTime.fromMillisecondsSinceEpoch(137 * 432000 * 1000 + 1506203091, isUtc: true);
+    //   final now1 = epochDateTime.encode(137);
+    //   final epoch = epochDateTime.decode(now1);
+    //   final now2 = epochDateTime.encode(epoch);
+    //   print("$now1 -> epochDateTime: -> $epoch");
+    //   expect(now1, equals(now2));
+    // });
+    test('epoch to unix time milliseconds', () {
+      final _epoch0Slot0Utc = epochToDateTime(epoch: 0);
+      expect(_epoch0Slot0Utc, epoch0Slot0Utc);
+
+      //last slot of epoch 207, slot 4492799, block 4490510, 2020/07/29 21:44:31
+      final epoch207UtcBase = DateTime(2020, 7, 24, 21, 44, 51).toUtc();
+      final epoch207Ut = epochToDateTime(epoch: 207);
+      expect(epoch207Ut, epoch207UtcBase);
+
+      //TODO post 207 epochs are off by 1 hour
+      //Epoch	Start Date	Stake Snapshot for Epoch	Rewards Paid for Epoch
+      //239	Thu 31 Dec 2020 (21:45:00 UTC)	240	237, 2020/12/31 21:44:51
+      final epoch239UtcBase = DateTime(2020, 12, 31, 20, 44, 51).toUtc();
+      final epoch239Utc = epochToDateTime(epoch: 239);
+      expect(epoch239Utc, epoch239UtcBase);
+
+      //Epoch	Start Date	Stake Snapshot for Epoch	Rewards Paid for Epoch
+      //309	Thu 16 Dec 2021 (21:45:00 UTC)	310	307
+      final epoch309UtcBase = DateTime(2021, 12, 16, 20, 44, 51).toUtc();
+      final epoch309Utc = epochToDateTime(epoch: 309);
+      expect(epoch309Utc, epoch309UtcBase);
+    });
+    test('slot to unix time milliseconds', () {
+      final epoch0Slot9UtcBase = DateTime(2017, 9, 23, 21, 47, 51).toUtc();
+      final epoch0Slot9Utc = slotToDateTime(slot: 9);
+      expect(epoch0Slot9Utc, epoch0Slot9UtcBase);
+
+      // https://explorer.cardano.org/en/epoch.html?number=136&page=0&perPage=10
+      // 136, 2937600, 2936067, 2019/08/04 21:44:51
+      final slot2937600UtcBase = DateTime(2019, 8, 4, 21, 44, 51).toUtc();
+      final slot2937600Utc = slotToDateTime(slot: 2937600);
+      expect(slot2937600Utc, slot2937600UtcBase);
+
+      // 278, 35164786, 6001557, 2021/07/19 21:44:37
+      final slot35164786UtcBase = DateTime(2021, 7, 19, 21, 44, 37).toUtc();
+      final slot35164786Utc = slotToDateTime(slot: 35164786);
+      expect(slot35164786Utc, slot35164786UtcBase);
+    });
+  });
 }
