b443b08c09bdfbea5b359d9ecc3b4c1a6a858375
Richard Easterling <richard@Richards-MacBook-Pro.local>
2021-07-24 11:31:14 -0700

    bech32 validation

---
+++ lib/src/util/bech32_validation.dart
@@ -0,0 +1,44 @@
+import 'package:oxidized/oxidized.dart';
+import 'package:quiver/strings.dart';
+
+///
+/// if bech32 string has the correct prefix, '1' seperator, legal data characters and optionaly correct length,
+/// the normalized correct form is returned. If it's not legal, then an explanation is return in the error message.
+///
+Result<String, String> validBech32({required String bech32, required List<String> hrpPrefixes, int? dataPartRequiredLength}) {
+  if (hrpPrefixes.isEmpty) {
+    throw Exception("validBech32 hrpPrefixes array must not be empty");
+  }
+  if (isBlank(bech32)) return Err("bech32 is missing");
+  final lowerCase = bech32.toLowerCase();
+  if (hrpPrefixes.length > 1) hrpPrefixes.sort((a, b) => b.compareTo(a)); //avoid matching 'addr' for 'addr_test'
+  print(hrpPrefixes);
+  final prefix = hrpPrefixes.firstWhere((prefix) => lowerCase.startsWith(prefix), orElse: () => '');
+  if (isBlank(prefix)) return Err("bech32 must start with ${hrpPrefixes.join(' or ')}");
+  if (lowerCase.length > prefix.length && lowerCase.codeUnitAt(prefix.length) != '1'.codeUnitAt(0))
+    return Err("invalid bech32 format, missing '1' after prefix");
+  final data = lowerCase.substring(prefix.length + 1);
+  if (dataPartRequiredLength != null && data.length != dataPartRequiredLength)
+    return Err("invalid bech32 data '$data' length, requires $dataPartRequiredLength but was ${data.length} characters long");
+  int invalidCharIndex = _firstIllegalDataChar(data);
+  if (invalidCharIndex > -1) return Err("invalid bech32 data '$data' character: ${data.substring(invalidCharIndex, invalidCharIndex + 1)}");
+  return Ok(lowerCase);
+}
+
+int _firstIllegalDataChar(String bech32Data) {
+  for (int i = 0; i < bech32Data.length; i++) {
+    if (!_legalBech32Char(bech32Data.codeUnitAt(i))) return i;
+  }
+  return -1;
+}
+
+bool _legalBech32Char(int ch16) => _indexOfCodeUnit(ch16, _bech32LegalDataChars) > -1;
+
+int _indexOfCodeUnit(int ch16, List<int> codeUnits) {
+  for (int i = 0; i < codeUnits.length; i++) {
+    if (ch16 == codeUnits[i]) return i;
+  }
+  return -1;
+}
+
+final _bech32LegalDataChars = '023456789acdefghjklmnpqrstuvwxyz'.codeUnits; //exlucdes '1bio'
---
+++ test/bech32_validation_test.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:cardano_wallet_sdk/src/util/bech32_validation.dart';
+import 'package:test/test.dart';
+//import 'dart:convert';
+
+void main() {
+  group('bech32 validation - ', () {
+    test('valid', () {
+      final result =
+          validBech32(bech32: 'addr_test1234567890acdefghjklmnpqrstuvwxyz', hrpPrefixes: ['addr', 'addr_test'], dataPartRequiredLength: 32);
+      expect(result.unwrap(), 'addr_test1234567890acdefghjklmnpqrstuvwxyz');
+    });
+    test('return lower case alphas', () {
+      final result =
+          validBech32(bech32: 'addr_test1234567890ACDEFGHJKLMNPQRSTUVWXYZ', hrpPrefixes: ['addr_test', 'addr'], dataPartRequiredLength: 32);
+      expect(result.unwrap(), 'addr_test1234567890acdefghjklmnpqrstuvwxyz');
+    });
+    test('invalid length', () {
+      final result =
+          validBech32(bech32: 'addr_test1234567890acdefghjklmnpqrstuvwxyz', hrpPrefixes: ['addr_test', 'addr'], dataPartRequiredLength: 31);
+      expect(result.isErr(), isTrue);
+      print(result.unwrapErr());
+    });
+    test('missing', () {
+      final result = validBech32(bech32: '', hrpPrefixes: ['addr_test', 'addr']);
+      expect(result.isErr(), isTrue);
+      print(result.unwrapErr());
+    });
+    test('invalid data char', () {
+      final result = validBech32(bech32: 'addr1234567890abcdefghjklmnpqrstuvwxyz', hrpPrefixes: ['addr_test', 'addr']);
+      expect(result.isErr(), isTrue);
+      print(result.unwrapErr());
+    });
+    test('invalid prefix', () {
+      final result = validBech32(bech32: 'dude_test1234567890acdefghjklmnpqrstuvwxyz', hrpPrefixes: ['addr_test', 'addr']);
+      expect(result.isErr(), isTrue);
+      print(result.unwrapErr());
+    });
+    test('missing 1 seperator', () {
+      final result = validBech32(bech32: 'addr234567890acdefghjklmnpqrstuvwxyz', hrpPrefixes: ['addr_test', 'addr']);
+      expect(result.isErr(), isTrue);
+      print(result.unwrapErr());
+    });
+  });
+}
