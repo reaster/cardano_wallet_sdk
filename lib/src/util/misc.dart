a25ed2800af1a15198554483c93f6fbd8221154e
reaster <richard@outsourcecafe.com>
2021-10-09 21:20:03 -0700

    misc common code

---
+++ lib/src/util/misc.dart
@@ -0,0 +1,2 @@
+/// Dump byte array. Example: bytes[20]: 244,155,227,187,150,186,199,61,202,241,76,208,46,192,219,56,241,103,253,67
+String b2s(List<int> bytes, {String prefix = 'bytes'}) => "$prefix[${bytes.length}]: ${bytes.join(',')}";
