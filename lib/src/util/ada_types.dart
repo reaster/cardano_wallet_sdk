f9c5dc09595ebda2db80e7a75c44db84d8ffb665
reaster <richard@outsourcecafe.com>
2021-10-26 02:03:45 -0700

    add AssetId as unit type

--- lib/src/util/ada_types.dart
+++ lib/src/util/ada_types.dart
@@ -4,3 +4,8 @@
 /// TODO migrate to BigInt in second beta release.
 ///
 typedef Coin = int;
+
+const Coin coinZero = 0;
+
+/// Native Token policyId+coinName. Alternatly called 'unit'.
+typedef AssetId = String;
