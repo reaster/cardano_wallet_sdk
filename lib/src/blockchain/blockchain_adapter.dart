872e2b9e220564473259bd41536bfe8c95ca96c2
reaster <richard@outsourcecafe.com>
2021-10-27 22:49:05 -0700

    extends BlockchainCache

--- lib/src/blockchain/blockchain_adapter.dart
+++ lib/src/blockchain/blockchain_adapter.dart
@@ -1,12 +1,13 @@
 import 'package:cardano_wallet_sdk/cardano_wallet_sdk.dart';
 import 'package:cardano_wallet_sdk/src/address/shelley_address.dart';
+import 'package:cardano_wallet_sdk/src/blockchain/blockchain_cache.dart';
 import 'package:cardano_wallet_sdk/src/wallet/wallet_factory.dart';
 import 'package:oxidized/oxidized.dart';
 
 ///
 /// High-level abstraction to blockchain tailored towards balences and transactions.
 ///
-abstract class BlockchainAdapter {
+abstract class BlockchainAdapter extends BlockchainCache {
   Future<Result<WalletUpdate, String>> updateWallet({required ShelleyAddress stakeAddress});
   Future<Result<Block, String>> latestBlock();
   Future<Result<String, String>> submitTransaction(List<int> cborTransaction);
