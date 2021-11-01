67bc7121e80ea8b1f98b977bed1aeabf2e86e473
reaster <richard@outsourcecafe.com>
2021-11-01 07:51:10 -0700

    removing...

--- lib/src/wallet/impl/wallet_factory_impl.dart
+++ lib/src/wallet/impl/wallet_factory_impl.dart
@@ -6,6 +6,7 @@ import 'package:cardano_wallet_sdk/src/address/hd_wallet.dart';
 import 'package:cardano_wallet_sdk/src/address/shelley_address.dart';
 import 'package:cardano_wallet_sdk/src/blockchain/blockchain_adapter_factory.dart';
 import 'package:cardano_wallet_sdk/src/network/network_id.dart';
+import 'package:cardano_wallet_sdk/src/transaction/coin_selection.dart';
 import 'package:cardano_wallet_sdk/src/wallet/impl/read_only_wallet_impl.dart';
 import 'package:cardano_wallet_sdk/src/wallet/impl/wallet_impl.dart';
 import 'package:cardano_wallet_sdk/src/wallet/read_only_wallet.dart';
@@ -18,144 +19,141 @@ import 'package:bip39/bip39.dart' as bip39;
 ///
 /// Shelley wallet factory provides various ways to create read-only or transactional wallets.
 ///
-class ShelleyWalletFactory extends BlockchainAdapterFactory implements WalletFactory {
-  Map<String, ReadOnlyWalletImpl> _walletCache = {};
-  // Map<NetworkId, CardanoNetwork> _networkMap =
-  //     Map.fromEntries(NetworkId.values.map((id) => CardanoNetwork.network(id)).map((n) => MapEntry(n.networkId, n)));
-  int _walletIndex = 0;
+// @Deprecated("use WalletBuilder")
+// class ShelleyWalletFactory extends BlockchainAdapterFactory implements WalletFactory {
+//   // TODO remove cache, factory should be stateless.
+//   Map<String, ReadOnlyWalletImpl> _walletCache = {};
+//   int _walletIndex = 0;
+//   CoinSelectionAlgorithm _coinSelectionFunction = largestFirst;
 
-  ShelleyWalletFactory({required Interceptor authInterceptor, required NetworkId networkId})
-      : super(authInterceptor: authInterceptor, networkId: networkId);
+//   ShelleyWalletFactory({required Interceptor authInterceptor, required NetworkId networkId})
+//       : super(authInterceptor: authInterceptor, networkId: networkId);
 
-  /// creates an factory given a authorization key
-  factory ShelleyWalletFactory.fromKey({required String key, required NetworkId networkId}) => ShelleyWalletFactory(
-      authInterceptor: BlockchainAdapterFactory.interceptorFromKey(key: key), networkId: networkId);
+//   /// creates an factory given a authorization key
+//   factory ShelleyWalletFactory.fromKey({required String key, required NetworkId networkId}) => ShelleyWalletFactory(
+//       authInterceptor: BlockchainAdapterFactory.interceptorFromKey(key: key), networkId: networkId);
 
-  @override
-  Future<Result<ReadOnlyWallet, String>> createReadOnlyWallet({
-    required ShelleyAddress stakeAddress,
-    String? walletName,
-    bool load = true,
-  }) async {
-    ReadOnlyWalletImpl? wallet = _walletCache[stakeAddress];
-    if (wallet != null) {
-      return Err("wallet already exists: '${wallet.walletName}'");
-    }
-    final String name = walletName ?? "Wallet #${++_walletIndex}";
-    wallet = ReadOnlyWalletImpl(
-      blockchainAdapter: adapter(),
-      stakeAddress: stakeAddress,
-      walletName: name,
-    );
-    if (load) {
-      try {
-        final result = await updateWallet(wallet: wallet);
-        if (result.isErr()) {
-          return Err(result.unwrapErr());
-        }
-      } catch (e) {
-        print(e);
-        return Err(e.toString());
-      }
-    }
-    _walletCache[stakeAddress.toBech32()] = wallet;
-    return Ok(wallet);
-  }
+//   @override
+//   Future<Result<ReadOnlyWallet, String>> createReadOnlyWallet({
+//     required ShelleyAddress stakeAddress,
+//     String? walletName,
+//     bool load = true,
+//   }) async {
+//     ReadOnlyWalletImpl? wallet = _walletCache[stakeAddress];
+//     if (wallet != null) {
+//       return Err("wallet already exists: '${wallet.walletName}'");
+//     }
+//     final String name = walletName ?? "Wallet #${++_walletIndex}";
+//     wallet = ReadOnlyWalletImpl(
+//       blockchainAdapter: adapter(),
+//       stakeAddress: stakeAddress,
+//       walletName: name,
+//     );
+//     if (load) {
+//       try {
+//         final result = await updateWallet(wallet: wallet);
+//         if (result.isErr()) {
+//           return Err(result.unwrapErr());
+//         }
+//       } catch (e) {
+//         print(e);
+//         return Err(e.toString());
+//       }
+//     }
+//     _walletCache[stakeAddress.toBech32()] = wallet;
+//     return Ok(wallet);
+//   }
 
-  @override
-  Future<Result<bool, String>> updateWallet({required ReadOnlyWallet wallet}) async {
-    bool changed = false;
-    bool error = false;
-    final blockchainAdapter = adapter();
-    final result = await blockchainAdapter.updateWallet(stakeAddress: wallet.stakeAddress);
-    result.when(
-      ok: (update) {
-        changed = wallet.refresh(
-            balance: update.balance,
-            transactions: update.transactions,
-            usedAddresses: update.addresses,
-            assets: update.assets,
-            stakeAccounts: update.stakeAccounts);
-      },
-      err: (err) {
-        error = true;
-      },
-    );
-    return error ? Err(result.unwrapErr()) : Ok(changed);
-  }
+//   @override
+//   Future<Result<bool, String>> updateWallet({required ReadOnlyWallet wallet}) async {
+//     bool changed = false;
+//     bool error = false;
+//     final blockchainAdapter = adapter();
+//     final result = await blockchainAdapter.updateWallet(stakeAddress: wallet.stakeAddress);
+//     result.when(
+//       ok: (update) {
+//         changed = wallet.refresh(
+//             balance: update.balance,
+//             transactions: update.transactions,
+//             usedAddresses: update.addresses,
+//             assets: update.assets,
+//             stakeAccounts: update.stakeAccounts);
+//       },
+//       err: (err) {
+//         error = true;
+//       },
+//     );
+//     return error ? Err(result.unwrapErr()) : Ok(changed);
+//   }
 
-  @override
-  ReadOnlyWallet? byStakeAddress(String stakeAddress) => _walletCache[stakeAddress];
+//   @override
+//   set coinSelectionAlgorithm(CoinSelectionAlgorithm coinSelectionAlgorithm) =>
+//       _coinSelectionFunction = coinSelectionAlgorithm;
 
-  @override
-  Future<Result<Wallet, String>> createWallet({
-    required HdWallet hdWallet,
-    String? walletName,
-    bool load = true,
-  }) async {
-    // print("hdWallet.rootSigningKey: ${hdWallet.rootSigningKey.encode()}");
-    // print("hdWallet.rootVerifyKey:  ${hdWallet.rootVerifyKey.encode()}");
-    final stakeKeyPair = hdWallet.deriveAddressKeys(role: stakingRole);
-    // final verifyKey = stakeKeyPair.verifyKey!;
-    // print("stakeKeyPair: ${verifyKey.encode()}");
-    // if (verifyKey.encode() !=
-    //     'ed25519bip32_pk18qvnk9eppdf0qnl7csz5h7lwdhf4jhjhu6x8a6m7fmmgz94zu9pv5xkazdaavyeq0xv9lz7cpj3u4yz5q5p4wk3hksppsskdrkm6ucq3m3kde')
-    //   return Err('verifyKey fail');
-    final stakeAddress = hdWallet.toRewardAddress(spend: stakeKeyPair.verifyKey!, networkId: networkId);
-    // final addr0KeyPair = hdWallet.deriveAddressKeys();
-    // final stakeAddress = hdWallet.toRewardAddress(spend: addr0KeyPair.verifyKey!, networkId: networkId);
-    ReadOnlyWalletImpl? wallet = _walletCache[stakeAddress];
-    if (wallet != null) {
-      if (wallet is WalletImpl) {
-        return Err("wallet already exists: '${wallet.walletName}'");
-      }
-      if (walletName == null) {
-        walletName = wallet.walletName;
-      }
-      _walletCache.remove(stakeAddress);
-    }
-    final String name = walletName ?? "Wallet #${++_walletIndex}";
-    wallet = WalletImpl(
-      blockchainAdapter: adapter(),
-      stakeAddress: stakeAddress,
-      walletName: name,
-      hdWallet: hdWallet,
-    );
-    if (load) {
-      try {
-        final result = await updateWallet(wallet: wallet);
-        if (result.isErr()) {
-          return Err(result.unwrapErr());
-        }
-      } catch (e) {
-        print(e);
-        return Err(e.toString());
-      }
-    }
-    _walletCache[stakeAddress.toBech32()] = wallet;
-    return Ok(wallet);
-  }
+//   @override
+//   ReadOnlyWallet? byStakeAddress(String stakeAddress) => _walletCache[stakeAddress];
 
-  @override
-  Future<Result<Wallet, String>> createWalletFromMnemonic({
-    required List<String> mnemonic,
-    String? walletName,
-    bool load = true,
-  }) async {
-    final hdWallet = HdWallet.fromMnemonic(mnemonic.join(' '));
-    return createWallet(hdWallet: hdWallet, walletName: walletName, load: load);
-  }
+//   @override
+//   Future<Result<Wallet, String>> createWallet({
+//     required HdWallet hdWallet,
+//     String? walletName,
+//     bool load = true,
+//   }) async {
+//     final stakeKeyPair = hdWallet.deriveAddressKeys(role: stakingRole);
+//     final stakeAddress = hdWallet.toRewardAddress(spend: stakeKeyPair.verifyKey!, networkId: networkId);
+//     ReadOnlyWalletImpl? wallet = _walletCache[stakeAddress];
+//     if (wallet != null) {
+//       if (wallet is WalletImpl) {
+//         return Err("wallet already exists: '${wallet.walletName}'");
+//       }
+//       if (walletName == null) {
+//         walletName = wallet.walletName;
+//       }
+//       _walletCache.remove(stakeAddress);
+//     }
+//     final String name = walletName ?? "Wallet #${++_walletIndex}";
+//     wallet = WalletImpl(
+//       blockchainAdapter: adapter(),
+//       stakeAddress: stakeAddress,
+//       walletName: name,
+//       hdWallet: hdWallet,
+//       coinSelectionFunction: _coinSelectionFunction,
+//     );
+//     if (load) {
+//       try {
+//         final result = await updateWallet(wallet: wallet);
+//         if (result.isErr()) {
+//           return Err(result.unwrapErr());
+//         }
+//       } catch (e) {
+//         print(e);
+//         return Err(e.toString());
+//       }
+//     }
+//     _walletCache[stakeAddress.toBech32()] = wallet;
+//     return Ok(wallet);
+//   }
 
-  @override
-  Future<Result<Wallet, String>> createWalletFromPrivateKey({
-    required Bip32SigningKey rootSigningKey,
-    String? walletName,
-    bool load = true,
-  }) {
-    final hdWallet = HdWallet(rootSigningKey: rootSigningKey);
-    return createWallet(hdWallet: hdWallet, walletName: walletName, load: load);
-  }
+//   @override
+//   Future<Result<Wallet, String>> createWalletFromMnemonic({
+//     required List<String> mnemonic,
+//     String? walletName,
+//     bool load = true,
+//   }) async {
+//     final hdWallet = HdWallet.fromMnemonic(mnemonic.join(' '));
+//     return createWallet(hdWallet: hdWallet, walletName: walletName, load: load);
+//   }
 
-  @override
-  List<String> generateNewMnemonic() => (bip39.generateMnemonic(strength: 256)).split(' ');
-}
+//   @override
+//   Future<Result<Wallet, String>> createWalletFromPrivateKey({
+//     required Bip32SigningKey rootSigningKey,
+//     String? walletName,
+//     bool load = true,
+//   }) {
+//     final hdWallet = HdWallet(rootSigningKey: rootSigningKey);
+//     return createWallet(hdWallet: hdWallet, walletName: walletName, load: load);
+//   }
+
+//   @override
+//   List<String> generateNewMnemonic() => (bip39.generateMnemonic(strength: 256)).split(' ');
+// }
--- lib/src/wallet/wallet_factory.dart
+++ lib/src/wallet/wallet_factory.dart
@@ -4,14 +4,11 @@
 import 'package:bip32_ed25519/api.dart';
 import 'package:cardano_wallet_sdk/src/address/hd_wallet.dart';
 import 'package:cardano_wallet_sdk/src/address/shelley_address.dart';
-import 'package:cardano_wallet_sdk/src/stake/stake_account.dart';
-import 'package:cardano_wallet_sdk/src/transaction/transaction.dart';
+import 'package:cardano_wallet_sdk/src/transaction/coin_selection.dart';
 import 'package:cardano_wallet_sdk/src/wallet/impl/read_only_wallet_impl.dart';
 import 'package:cardano_wallet_sdk/src/wallet/read_only_wallet.dart';
 import 'package:cardano_wallet_sdk/src/wallet/wallet.dart';
 import 'package:oxidized/oxidized.dart';
-import 'package:cardano_wallet_sdk/src/asset/asset.dart';
-import 'package:cardano_wallet_sdk/src/util/ada_types.dart';
 
 ///
 /// Creates wallets from keys.
@@ -19,63 +16,49 @@ import 'package:cardano_wallet_sdk/src/util/ada_types.dart';
 /// Wallets arc cached and can be looked up from their stakingAddress.
 ///
 ///
-abstract class WalletFactory {
-  ///Create Cardano wallet given a 24 word mnemonic and optional wallet name.
-  ///If load is true, wallet will sync with blockchain.
-  Future<Result<Wallet, String>> createWalletFromMnemonic({
-    required List<String> mnemonic,
-    String? walletName,
-    bool load = true,
-  });
+// @Deprecated("use WalletBuilder")
+// abstract class WalletFactory {
+//   ///Create Cardano wallet given a 24 word mnemonic and optional wallet name.
+//   ///If load is true, wallet will sync with blockchain.
+//   Future<Result<Wallet, String>> createWalletFromMnemonic({
+//     required List<String> mnemonic,
+//     String? walletName,
+//     bool load = true,
+//   });
 
-  ///create Cardano wallet given a private key and optional wallet name.
-  ///If load is true, wallet will sync with blockchain.
-  Future<Result<Wallet, String>> createWalletFromPrivateKey({
-    required Bip32SigningKey rootSigningKey,
-    String? walletName,
-    bool load = true,
-  });
+//   ///create Cardano wallet given a private key and optional wallet name.
+//   ///If load is true, wallet will sync with blockchain.
+//   Future<Result<Wallet, String>> createWalletFromPrivateKey({
+//     required Bip32SigningKey rootSigningKey,
+//     String? walletName,
+//     bool load = true,
+//   });
 
-  ///Create Cardano wallet given a HdWallet instance and optional wallet name.
-  ///If load is true, wallet will sync with blockchain.
-  Future<Result<Wallet, String>> createWallet({
-    required HdWallet hdWallet,
-    String? walletName,
-    bool load = true,
-  });
+//   ///Create Cardano wallet given a HdWallet instance and optional wallet name.
+//   ///If load is true, wallet will sync with blockchain.
+//   Future<Result<Wallet, String>> createWallet({
+//     required HdWallet hdWallet,
+//     String? walletName,
+//     bool load = true,
+//   });
 
-  ///create Cardano wallet given a stakeAddress, networkId and optional wallet name.
-  ///If load is true, wallet will sync with blockchain.
-  Future<Result<ReadOnlyWallet, String>> createReadOnlyWallet({
-    required ShelleyAddress stakeAddress,
-    String? walletName,
-    bool load = true,
-  });
+//   ///create Cardano wallet given a stakeAddress, networkId and optional wallet name.
+//   ///If load is true, wallet will sync with blockchain.
+//   Future<Result<ReadOnlyWallet, String>> createReadOnlyWallet({
+//     required ShelleyAddress stakeAddress,
+//     String? walletName,
+//     bool load = true,
+//   });
 
-  ///update existing wallet by syncing with blockchain.
-  Future<Result<bool, String>> updateWallet({required ReadOnlyWalletImpl wallet});
+//   ///update existing wallet by syncing with blockchain.
+//   Future<Result<bool, String>> updateWallet({required ReadOnlyWalletImpl wallet});
 
-  ///lookup cached wallet by stakingAddress
-  ReadOnlyWallet? byStakeAddress(String stakeAddress);
+//   ///lookup cached wallet by stakingAddress
+//   ReadOnlyWallet? byStakeAddress(String stakeAddress);
 
-  ///generate mnumonic words to be used by new wallet.
-  List<String> generateNewMnemonic();
-}
+//   ///generate mnumonic words to be used by new wallet.
+//   List<String> generateNewMnemonic();
 
-///
-/// data object allowing existing or new wallet to be updated
-///
-class WalletUpdate {
-  final Coin balance;
-  final List<RawTransaction> transactions;
-  final List<ShelleyAddress> addresses;
-  final Map<String, CurrencyAsset> assets;
-  final List<StakeAccount> stakeAccounts;
-  WalletUpdate({
-    required this.balance,
-    required this.transactions,
-    required this.addresses,
-    required this.assets,
-    required this.stakeAccounts,
-  });
-}
+//   ///set default CoinSelectionAlgorithm
+//   set coinSelectionAlgorithm(CoinSelectionAlgorithm coinSelectionAlgorithm);
+// }
