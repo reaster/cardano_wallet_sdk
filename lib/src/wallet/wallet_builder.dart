0330c57e8a2b15e1ecb8360d5f87489887a79d9c
reaster <richard@outsourcecafe.com>
2021-11-01 07:55:04 -0700

    wallet builder replaces factory

---
+++ lib/src/wallet/wallet_builder.dart
@@ -0,0 +1,173 @@
+// Copyright 2021 Richard Easterling
+// SPDX-License-Identifier: Apache-2.0
+
+import 'package:bip32_ed25519/api.dart';
+import 'package:cardano_wallet_sdk/cardano_wallet_sdk.dart';
+import 'package:cardano_wallet_sdk/src/address/hd_wallet.dart';
+import 'package:cardano_wallet_sdk/src/transaction/coin_selection.dart';
+import 'package:cardano_wallet_sdk/src/wallet/wallet.dart';
+import 'package:dio/dio.dart';
+import 'package:oxidized/oxidized.dart';
+import 'package:bip39/bip39.dart' as bip39;
+import 'package:bip32_ed25519/bip32_ed25519.dart';
+import 'package:pinenacl/key_derivation.dart';
+
+///
+/// Creates wallets from provided properties.
+///
+class WalletBuilder {
+  NetworkId? _networkId;
+  String? _walletName;
+  BlockchainAdapter? _adapter;
+  String? _testnetAdapterKey;
+  String? _mainnetAdapterKey;
+  ShelleyAddress? _stakeAddress;
+  List<String>? _mnemonic;
+  // Uint8List? _seed;
+  Bip32SigningKey? _rootSigningKey;
+  // Bip32SigningKey? _addressKeyPair;
+  int accountIndex = defaultAccountIndex;
+  HdWallet? _hdWallet;
+  CoinSelectionAlgorithm _coinSelectionFunction = largestFirst;
+
+  static int _walletNameIndex = 1;
+
+  set stakeAddress(ShelleyAddress stakeAddress) => _stakeAddress = stakeAddress;
+
+  set testnetAdapterKey(String? testnetAdapterKey) => _testnetAdapterKey = testnetAdapterKey;
+
+  set mainnetAdapterKey(String? mainnetAdapterKey) => _mainnetAdapterKey = mainnetAdapterKey;
+
+  set adapter(BlockchainAdapter adapter) => _adapter = adapter;
+
+  set walletName(String? walletName) => _walletName = walletName;
+
+  set networkId(NetworkId? networkId) => _networkId = networkId;
+
+  set hdWallet(HdWallet hdWallet) => _hdWallet = hdWallet;
+
+  set rootSigningKey(Bip32SigningKey? rootSigningKey) => _rootSigningKey = rootSigningKey;
+
+  // set seed(Uint8List seed) => _seed = seed;
+
+  set mnemonic(List<String> mnemonic) => _mnemonic = mnemonic;
+
+  set coinSelectionAlgorithm(CoinSelectionAlgorithm coinSelectionAlgorithm) =>
+      _coinSelectionFunction = coinSelectionAlgorithm;
+
+  Result<ReadOnlyWallet, String> readOnlyBuild() {
+    if (_stakeAddress == null) return Err("Read-only wallet creation requires a staking address");
+    if (_walletName == null) {
+      _walletName = "Wallet #${_walletNameIndex++}";
+    }
+    if (_adapter != null) {
+      if (_adapter! is BlockfrostBlockchainAdapter) {
+        _networkId = (_adapter as BlockfrostBlockchainAdapter).networkId;
+      }
+    } else {
+      if (_networkId == null) _networkId = NetworkId.mainnet;
+      final adapterResult = _lookupOrCreateBlockchainAdapter(
+        networkId: _networkId!,
+        key: _networkId! == NetworkId.mainnet ? _mainnetAdapterKey : _testnetAdapterKey,
+      );
+      if (adapterResult.isErr()) return Err(adapterResult.unwrapErr());
+      _adapter = adapterResult.unwrap();
+    }
+
+    final wallet = ReadOnlyWalletImpl(
+      blockchainAdapter: _adapter!,
+      stakeAddress: _stakeAddress!,
+      walletName: _walletName!,
+    );
+    return Ok(wallet);
+  }
+
+  Future<Result<ReadOnlyWallet, String>> readOnlyBuildAndSync() async {
+    final walletResult = readOnlyBuild();
+    if (walletResult.isErr()) return Err(walletResult.unwrapErr());
+    final wallet = walletResult.unwrap();
+    final updateResult = await wallet.update();
+    if (updateResult.isErr()) return Err(updateResult.unwrapErr());
+    return Ok(wallet);
+  }
+
+  Result<Wallet, String> build() {
+    if (_hdWallet != null) {
+      _rootSigningKey = _hdWallet!.rootSigningKey;
+    } else {
+      if (_mnemonic == null) return Err("wallet creation requires a mnemonic phrase.");
+      _hdWallet = HdWallet.fromMnemonic(_mnemonic!.join(' ')); //(rootSigningKey: _rootSigningKey!);
+      _rootSigningKey = _hdWallet!.rootSigningKey;
+      // if (_rootSigningKey == null) {
+      //   if (_mnemonic != null) {
+      //     _seed = bip39.mnemonicToSeed(_mnemonic!.join(' '));
+      //   }
+      //   if (_seed != null) {
+      //     _rootSigningKey = _bip32signingKey(_seed!);
+      //   }
+      // }
+      // if (_rootSigningKey == null) return Err("wallet creation requires a 'rootPrivateKey', '_mnemonic' or 'seed'");
+      // _hdWallet = HdWallet(rootSigningKey: _rootSigningKey!);
+    }
+    if (_walletName == null) {
+      _walletName = "Wallet #${_walletNameIndex++}";
+    }
+    if (_adapter != null) {
+      if (_adapter! is BlockfrostBlockchainAdapter) {
+        _networkId = (_adapter as BlockfrostBlockchainAdapter).networkId;
+      }
+    } else {
+      if (_networkId == null) _networkId = NetworkId.mainnet;
+      final adapterResult = _lookupOrCreateBlockchainAdapter(
+        networkId: _networkId!,
+        key: _networkId! == NetworkId.mainnet ? _mainnetAdapterKey : _testnetAdapterKey,
+      );
+      if (adapterResult.isErr()) return Err(adapterResult.unwrapErr());
+      _adapter = adapterResult.unwrap();
+    }
+    final stakeKeyPair = _hdWallet!.deriveAddressKeys(role: stakingRole);
+    final stakeAddress = _hdWallet!.toRewardAddress(spend: stakeKeyPair.verifyKey!, networkId: _networkId!);
+    final addressKeyPair = _hdWallet!.deriveAddressKeys(account: accountIndex);
+    //printKey(addressKeyPair);
+    final wallet = WalletImpl(
+      blockchainAdapter: _adapter!,
+      stakeAddress: stakeAddress,
+      addressKeyPair: addressKeyPair,
+      walletName: _walletName!,
+      hdWallet: _hdWallet!,
+      coinSelectionFunction: _coinSelectionFunction,
+    );
+    return Ok(wallet);
+  }
+
+  Future<Result<Wallet, String>> buildAndSync() async {
+    final walletResult = build();
+    if (walletResult.isErr()) return Err(walletResult.unwrapErr());
+    final wallet = walletResult.unwrap();
+    final updateResult = await wallet.update();
+    if (updateResult.isErr()) return Err(updateResult.unwrapErr());
+    return Ok(wallet);
+  }
+
+  ///generate mnumonic words to be used by new wallet.
+  List<String> generateNewMnemonic() => (bip39.generateMnemonic(strength: 256)).split(' ');
+
+  static Map<NetworkId, BlockchainAdapterFactory> _blockchainAdapterFactoryCache = {};
+  static Map<NetworkId, BlockchainAdapter> _blockchainAdapterCache = {};
+
+  static Result<BlockchainAdapter, String> _lookupOrCreateBlockchainAdapter(
+      {required NetworkId networkId, String? key}) {
+    BlockchainAdapter? adapter = _blockchainAdapterCache[networkId];
+    if (adapter != null) return Ok(adapter);
+    BlockchainAdapterFactory? factory = _blockchainAdapterFactoryCache[networkId];
+    if (factory == null) {
+      if (key == null) return Err("no BlockFrost key supplied for ${networkId.toString()} network");
+      Interceptor authInterceptor = BlockchainAdapterFactory.interceptorFromKey(key: key);
+      factory = BlockchainAdapterFactory(authInterceptor: authInterceptor, networkId: networkId);
+      _blockchainAdapterFactoryCache[networkId] = factory;
+    }
+    adapter = factory!.adapter();
+    _blockchainAdapterCache[networkId] = adapter;
+    return Ok(adapter);
+  }
+}
--- test/read_only_wallet_test.dart
+++ test/read_only_wallet_test.dart
@@ -1,16 +1,9 @@
 // Copyright 2021 Richard Easterling
 // SPDX-License-Identifier: Apache-2.0
 
-import 'package:cardano_wallet_sdk/src/address/shelley_address.dart';
-import 'package:cardano_wallet_sdk/src/network/network_id.dart';
-import 'package:cardano_wallet_sdk/src/util/ada_formatter.dart';
-import 'package:cardano_wallet_sdk/src/wallet/impl/read_only_wallet_impl.dart';
-import 'package:cardano_wallet_sdk/src/wallet/impl/wallet_factory_impl.dart';
-import 'package:cardano_wallet_sdk/src/wallet/read_only_wallet.dart';
-import 'package:cardano_wallet_sdk/src/wallet/wallet_factory.dart';
+import 'package:cardano_wallet_sdk/cardano_wallet_sdk.dart';
 import 'package:test/test.dart';
 import './my_api_key_auth.dart';
-import 'package:cardano_wallet_sdk/src/asset/asset.dart';
 import 'package:oxidized/oxidized.dart';
 
 void main() {
@@ -18,43 +11,51 @@ void main() {
   final wallet2 = 'stake_test1uz425a6u2me7xav82g3frk2nmxhdujtfhmf5l275dr4a5jc3urkeg';
   final wallet3 = 'stake_test1upnk3u6wd65w7na3rkamznyzjspv7kgu7xm9j8w5m00xcls39m99d';
   final wallet4 = 'stake_test1uqhwfumjye2t99ekdq02njm0wsdz84pmd0h2cxrg4napshs0uedxa';
-
-  final walletFactory = ShelleyWalletFactory(authInterceptor: MyApiKeyAuthInterceptor(), networkId: NetworkId.testnet);
+  final interceptor = MyApiKeyAuthInterceptor();
+  final builder = WalletBuilder()
+    ..networkId = NetworkId.testnet
+    ..testnetAdapterKey = interceptor.apiKey;
 
   group('PublicWallet -', () {
     test('test create testnet wallet 1', () async {
-      await testWallet(stakeAddress: wallet1, walletFactory: walletFactory);
+      await testWalletFromBuilder(builder
+        ..walletName = 'Wallet 1'
+        ..stakeAddress = ShelleyAddress.fromBech32(wallet1));
     });
     test('create testnet wallet 2', () async {
-      await testWallet(stakeAddress: wallet2, walletFactory: walletFactory, walletName: 'Wallet 2');
+      await testWalletFromBuilder(builder
+        ..walletName = 'Wallet 2'
+        ..stakeAddress = ShelleyAddress.fromBech32(wallet2));
     });
     test('create testnet wallet 3', () async {
-      await testWallet(stakeAddress: wallet3, walletFactory: walletFactory, walletName: 'Wallet 3');
+      await testWalletFromBuilder(builder
+        ..walletName = 'Wallet 3'
+        ..stakeAddress = ShelleyAddress.fromBech32(wallet3));
     });
     test('create testnet wallet 4', () async {
-      await testWallet(stakeAddress: wallet4, walletFactory: walletFactory, walletName: 'Wallet 4');
+      await testWalletFromBuilder(builder
+        ..walletName = 'Wallet 4'
+        ..stakeAddress = ShelleyAddress.fromBech32(wallet4));
     });
-    test('missing account', () async {
-      final result = await testWallet(
-          stakeAddress: 'stake1uy88uenysztnswv6u3cssgpamztc25q5wea703rnp50s4qq0ddctn',
-          walletFactory: walletFactory,
-          walletName: 'MIA');
-      expect(true, result.isErr());
-      print("ERROR: ${result.unwrapErr()}");
-    });
-    test('create mainnet wallet 8', () async {
-      final wallet8 = 'stake1uy88uenysztnswv6u3cssgpamztc25q5wea703rnp50s4qq0ddctn';
-      await testWallet(stakeAddress: wallet8, walletFactory: walletFactory, walletName: 'Fat Cat 8');
-    }, skip: 'mainnet auth not working');
+    //   test('missing account', () async {
+    //     final result = await testWallet(
+    //         stakeAddress: 'stake1uy88uenysztnswv6u3cssgpamztc25q5wea703rnp50s4qq0ddctn',
+    //         walletFactory: walletFactory,
+    //         walletName: 'MIA');
+    //     expect(true, result.isErr());
+    //     print("ERROR: ${result.unwrapErr()}");
+    //   });
+    //   test('create mainnet wallet 8', () async {
+    //     final wallet8 = 'stake1uy88uenysztnswv6u3cssgpamztc25q5wea703rnp50s4qq0ddctn';
+    //     await testWallet(stakeAddress: wallet8, walletFactory: walletFactory, walletName: 'Fat Cat 8');
+    //   }, skip: 'mainnet auth not working');
   });
 }
 
 final formatter = AdaFormattter.compactCurrency();
 
-Future<Result<ReadOnlyWallet, String>> testWallet(
-    {required String stakeAddress, required WalletFactory walletFactory, String? walletName}) async {
-  final result = await walletFactory.createReadOnlyWallet(
-      stakeAddress: ShelleyAddress.fromBech32(stakeAddress), walletName: walletName);
+Future<Result<ReadOnlyWallet, String>> testWalletFromBuilder(WalletBuilder builder) async {
+  final result = await builder.readOnlyBuildAndSync();
   bool error = false;
   result.when(
     ok: (wallet) {
@@ -80,14 +81,14 @@ Future<Result<ReadOnlyWallet, String>> testWallet(
     err: (err) {
       print(err);
       error = true;
+      expect(error, isFalse, reason: err);
       return Err(err);
     },
   );
   if (error) {
     return Err(result.unwrapErr());
   }
-  final wallet = walletFactory.byStakeAddress(stakeAddress);
-  final update = await walletFactory.updateWallet(wallet: wallet as ReadOnlyWalletImpl);
+  final update = await result.unwrap().update();
   expect(false, update.unwrap());
   return Ok(result.unwrap());
 }
