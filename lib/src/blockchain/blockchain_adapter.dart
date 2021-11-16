c495cfec89c7986e6be26ecb703d9a1e2b30fd4b
reaster <richard@outsourcecafe.com>
2021-11-16 00:14:48 -0800

    add tx methods and docs

--- lib/src/blockchain/blockchain_adapter.dart
+++ lib/src/blockchain/blockchain_adapter.dart
@@ -3,16 +3,27 @@
 
 import 'package:cardano_wallet_sdk/src/address/shelley_address.dart';
 import 'package:cardano_wallet_sdk/src/blockchain/blockchain_cache.dart';
+import 'package:cardano_wallet_sdk/src/transaction/min_fee_function.dart';
 import 'package:cardano_wallet_sdk/src/transaction/transaction.dart';
 import 'package:cardano_wallet_sdk/src/wallet/impl/wallet_update.dart';
 import 'package:oxidized/oxidized.dart';
+import 'dart:typed_data';
 
 ///
 /// High-level abstraction to blockchain tailored towards balences and transactions.
 ///
 abstract class BlockchainAdapter extends BlockchainCache {
+  /// Collects the latest transactions for the wallet given it's staking address.
   Future<Result<WalletUpdate, String>> updateWallet(
       {required ShelleyAddress stakeAddress});
+
+  /// Returns last latest Block instance from blockchain if successful.
   Future<Result<Block, String>> latestBlock();
-  Future<Result<String, String>> submitTransaction(List<int> cborTransaction);
+
+  /// Submit ShelleyTransaction encoded as CBOR. Returns hex transaction ID if successful.
+  Future<Result<String, String>> submitTransaction(Uint8List cborTransaction);
+
+  /// Return the fee parameters for the given epoch number or the latest epoch if no number supplied.
+  Future<Result<LinearFee, String>> latestEpochParameters(
+      {int epochNumber = 0});
 }
