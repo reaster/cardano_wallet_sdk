d4a423f898a36897c04117976e7f22656724d168
Richard Easterling <richard@Richards-MacBook-Pro.local>
2021-11-25 00:11:19 -0800

    add SingleChildScrollView for overflows

--- example/flutter_example/lib/src/widgets/alert_dialog.dart
+++ example/flutter_example/lib/src/widgets/alert_dialog.dart
@@ -5,29 +5,35 @@ Future<void> asyncAlertDialog(
   String title,
   String content,
 ) async {
-  final titleTheme =
-      Theme.of(context).textTheme.subtitle1!.apply(color: Colors.red);
-  final bodyTheme = Theme.of(context).textTheme.bodyText1;
   return await showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(title, style: titleTheme),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(content, style: bodyTheme),
+      context: context,
+      builder: (dialogContext) {
+        final titleTheme = Theme.of(dialogContext)
+            .textTheme
+            .subtitle1!
+            .apply(color: Colors.red);
+        final bodyTheme = Theme.of(dialogContext).textTheme.bodyText1;
+
+        return AlertDialog(
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(title, style: titleTheme),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(content, style: bodyTheme),
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
-      actions: [
-        ElevatedButton(
-          child: const Center(child: Text('Ok')),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-      ],
-    ),
-  );
+          actions: [
+            ElevatedButton(
+              child: const Center(child: Text('Ok')),
+              onPressed: () => Navigator.of(dialogContext).pop(),
+            ),
+          ],
+        );
+      });
 }
