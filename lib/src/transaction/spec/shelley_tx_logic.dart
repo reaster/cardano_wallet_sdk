f5538a5e9370aa52ca74a8722850ee2aeaa325ed
Richard Easterling <richard@Richards-MacBook-Pro.local>
2022-04-05 17:15:30 -0700

    fix array size issues

--- lib/src/transaction/spec/shelley_tx_logic.dart
+++ lib/src/transaction/spec/shelley_tx_logic.dart
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'package:bip32_ed25519/bip32_ed25519.dart';
-import '../../util/blake2bhash.dart';
-import '../../util/codec.dart';
-import './shelley_spec.dart';
+import '../../../cardano_wallet_sdk.dart';
+
+const int secretKeyLength = 32;
 
 ///
 /// Extends ShelleyTransaction to handle signature verification.
@@ -15,13 +15,20 @@ extension ShelleyTransactionLogic on ShelleyTransaction {
   ///
   bool get verify {
     for (ShelleyVkeyWitness witness in witnessSet!.vkeyWitnesses) {
-      final signature = Signature(Uint8List.fromList(
-          witness.signature.sublist(0, Signature.signatureLength)));
-      final verifyKey = Bip32VerifyKey(uint8ListFromBytes(witness.vkey));
+      final signature =
+          Signature(Uint8List.fromList(witness.signature.sublist(0, 64)));
+      // witness.signature.sublist(0, Signature.signatureLength)));
+      final verifyKey =
+          VerifyKey(uint8ListFromBytes(witness.vkey.sublist(0, 32)));
+      // final verifyKey = Bip32VerifyKey(uint8ListFromBytes(witness.vkey));
       final bodyData = body.toCborMap().getData();
       final List<int> hash = blake2bHash256(bodyData);
       Uint8List message = Uint8List.fromList(hash);
-      if (!verifyKey.verify(signature: signature, message: message)) {
+      // if (!verifyKey.verify(signature: signature, message: message)) {
+      if (!verifyEd25519(
+          signature: signature.asTypedList,
+          message: message,
+          publicKey: verifyKey.asTypedList)) {
         return false;
       }
     }
