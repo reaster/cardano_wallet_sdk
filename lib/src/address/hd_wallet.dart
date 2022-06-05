319ff40fe34d6c81b1e504ff739e1df5f7223b9f
reaster <richard@outsourcecafe.com>
2022-06-06 00:56:18 +0100

    migrate path tests

--- lib/src/address/hd_wallet.dart
+++ lib/src/address/hd_wallet.dart
@@ -313,119 +313,27 @@ class HdWallet {
       ShelleyAddress.toRewardAddress(spend: spend, networkId: networkId);
 }
 
-// class Segment {
-//   final int index;
-//   final bool harden;
-//   const Segment({required this.index, this.harden = false});
-//   int get value => harden ? index | hardenedOffset : index;
-//   @override
-//   String toString() => "$index${harden ? '\'' : ''}";
-//   Segment inc() => Segment(index: index + 1, harden: harden);
-// }
-
-// class DerivationChain {
-//   final String key;
-//   final List<Segment> segments;
-
-//   const DerivationChain({required this.key, required this.segments});
-
-//   DerivationChain.segments(this.key, Segment? seg1, Segment? seg2,
-//       Segment? seg3, Segment? seg4, Segment? seg5)
-//       : segments = [
-//           if (seg1 != null) seg1,
-//           if (seg2 != null) seg2,
-//           if (seg3 != null) seg3,
-//           if (seg4 != null) seg4,
-//           if (seg5 != null) seg5
-//         ];
-
-//   factory DerivationChain.fromPath(String path, {int? segmentLength}) =>
-//       DerivationChain(
-//           key: parseKey(path),
-//           segments: parsePath(path, segmentLength: segmentLength));
-
-//   static String parseKey(String path) {
-//     final key = path.substring(0, 1);
-//     if (!_legalPrefixes.contains(key)) {
-//       throw ArgumentError("DerivationChain must start with 'm' or 'M'", path);
-//     }
-//     return key;
-//   }
-
-//   static List<Segment> parsePath(String path, {int? segmentLength}) {
-//     List<Segment> segments = [];
-//     final tokens = path.split('/');
-//     segmentLength ??= tokens.length - 1;
-//     if (segmentLength != tokens.length - 1) {
-//       throw ArgumentError(
-//           "path must have $segmentLength segments, not ${tokens.length - 1}",
-//           path);
-//     }
-//     for (int i = 0; i < tokens.length; i++) {
-//       if (i > 0) {
-//         //ignore 0 key segment
-//         final harden = tokens[i].endsWith('\'');
-//         final seg =
-//             tokens[i].substring(0, tokens[i].length + (harden ? -1 : 0));
-//         final value = int.parse(seg);
-//         segments.add(Segment(index: value, harden: harden));
-//       }
-//     }
-//     return segments;
-//   }
-
-//   DerivationChain append(Segment tail) =>
-//       DerivationChain(key: key, segments: [...segments, tail]);
-//   DerivationChain append2(Segment tail1, Segment tail2) =>
-//       DerivationChain(key: key, segments: [...segments, tail1, tail2]);
-//   DerivationChain swapTail(Segment tail) => DerivationChain(
-//       key: key, segments: [...segments.take(segments.length - 1), tail]);
-
-//   /// increment the last value in the chain by one.
-//   DerivationChain inc() => DerivationChain(
-//       key: key,
-//       segments: segments.isEmpty
-//           ? []
-//           : [...segments.take(segments.length - 1), segments.last.inc()]);
-
-//   /// return BIP32 path string representation
-//   String toPath() => "$key/${segments.join('/')}";
-
-//   @override
-//   String toString() => toPath();
-
-//   bool get isPrivateRoot => key == _privateKeyPrefix;
-//   bool get isPublicRoot => key == _publicKeyPrefix;
-
-//   /// number of segments not incuding key
-//   int get length => segments.length;
-
-//   static const _privateKeyPrefix = 'm';
-//   static const _publicKeyPrefix = 'M';
-//   static const _legalPrefixes = [_privateKeyPrefix, _publicKeyPrefix];
-// }
-
 /// Cardano adoption of BIP-44 path:
 ///     m / 1852' / 1851' / account' / role / index
-@Deprecated('not sure this is needed or useful')
-class CIP1852Path extends DerivationChain {
-  CIP1852Path({
-    required key,
-    required Segment purpose,
-    required Segment coinType,
-    required Segment account,
-    required Segment role,
-    required Segment index,
-  }) : super(key: key, segments: [purpose, coinType, account, role, index]);
+// @Deprecated('not sure this is needed or useful')
+// class CIP1852Path extends DerivationChain {
+//   CIP1852Path({
+//     required key,
+//     required Segment purpose,
+//     required Segment coinType,
+//     required Segment account,
+//     required Segment role,
+//     required Segment index,
+//   }) : super(key: key, segments: [purpose, coinType, account, role, index]);
 
-  factory CIP1852Path.fromPath(String path) =>
-      DerivationChain.fromPath(path, segmentLength: 5) as CIP1852Path;
-  Segment get purpose => segments[0];
-  Segment get coinType => segments[1];
-  Segment get account => segments[2];
-  Segment get role => segments[3];
-  Segment get index => segments[4];
-}
+//   factory CIP1852Path.fromPath(String path) =>
+//       DerivationChain.fromPath(path, segmentLength: 5) as CIP1852Path;
+//   Segment get purpose => segments[0];
+//   Segment get coinType => segments[1];
+//   Segment get account => segments[2];
+//   Segment get role => segments[3];
+//   Segment get index => segments[4];
+// }
 
 /// Everything you need to add a spend (or change) address to a UTxO transaction.
 @Deprecated('not the way other Cardano SDKs handle keys and addresses')
