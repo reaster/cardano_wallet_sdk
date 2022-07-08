a202c2a37058ae53eef7229f702c2a0a88158e50
Richard Easterling <richard@Richards-MacBook-Pro.local>
2022-07-07 21:14:08 -0700

    signing

--- lib/src/transaction/model/bc_tx_ext.dart
+++ lib/src/transaction/model/bc_tx_ext.dart
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'package:bip32_ed25519/bip32_ed25519.dart';
+import 'package:pinenacl/tweetnacl.dart';
 import '../../../cardano_wallet_sdk.dart';
 import 'package:cbor/cbor.dart';
 
@@ -11,6 +12,48 @@ const int secretKeyLength = 32;
 /// Extends ShelleyTransaction to handle signature verification.
 ///
 extension BcTransactionLogic on BcTransaction {
+  ///
+  /// Given a signing key, return the signature and verify key as a witness.
+  /// If fakeSignature is true, just return a dummy signature for cost calculations.
+  /// TODO use signEd25519Extended function?
+  ///
+  BcVkeyWitness signedWitness(Bip32SigningKey signingKey,
+      {bool fakeSignature = false}) {
+    final bodyData = cbor.encode(body.toCborMap());
+    List<int> hash = blake2bHash256(bodyData);
+    final signedMessage =
+        fakeSignature ? _fakeSign(hash) : signingKey.sign(hash);
+    final witness = BcVkeyWitness(
+        vkey: signingKey.verifyKey.rawKey, signature: signedMessage.signature);
+    return witness;
+  }
+
+  ///
+  /// Give a list of signing keys, generate new transaction containing a witness set.
+  /// If fakeSignature is true, generate dummy signatures for cost calculations.
+  ///
+  BcTransaction sign(List<Bip32SigningKey> signingKeys,
+      {bool fakeSignature = false}) {
+    List<BcVkeyWitness> witnesses = signingKeys
+        .map((k) => signedWitness(k, fakeSignature: fakeSignature))
+        .toList();
+    final witnessSet =
+        BcTransactionWitnessSet(vkeyWitnesses: witnesses, nativeScripts: []);
+    return BcTransaction(
+        body: body,
+        isValid: isValid,
+        metadata: metadata,
+        witnessSet: witnessSet);
+  }
+
+  /// Generate fake signature that just has to be correct length for size calculation.
+  SignedMessage _fakeSign(List<int> message) {
+    var sm = Uint8List(message.length + TweetNaCl.signatureLength);
+    sm.fillRange(
+        0, sm.length, 42); //file fake sig with meaning of life & everything.
+    return SignedMessage.fromList(signedMessage: sm);
+  }
+
   ///
   /// Verify each witness in the witness set.
   ///
--- test/transaction/model/bc_tx_test.dart
+++ test/transaction/model/bc_tx_test.dart
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import 'package:cardano_wallet_sdk/cardano_wallet_sdk.dart';
+import 'package:bip32_ed25519/bip32_ed25519.dart';
 import 'package:cbor/cbor.dart';
 import 'dart:convert' as convertor;
 import 'package:test/test.dart';
@@ -100,6 +101,87 @@ void main() {
       //TODO fix expect(txHex, txHex2);
       //print(tx2.toJson(prettyPrint: true));
       //print(codec.decodedToJSON()); // [1,2,3],67.89,10,{"a":"a/ur1","b":1234567899,"c":"19/04/2020"},"^[12]g"
+    });
+
+    test('signPaymentTransactionMultiAccount', () {
+      final List<BcTransactionInput> inputs = [
+        BcTransactionInput(
+            transactionId:
+                '73198b7ad003862b9798106b88fbccfca464b1a38afb34958275c4a7d7d8d002',
+            index: 1), //long balance1 = 989264070;
+        BcTransactionInput(
+            transactionId:
+                '8e03a93578dc0acd523a4dd861793068a06a68b8a6c7358d0c965d2864067b68',
+            index: 0), //long balance2 = 1000000000;
+      ];
+      final fee = 367965;
+      final ttl = 26194586;
+      final balance1 = 989264070;
+      final amount1 = 5000000;
+      final changeAmount1 = balance1 - amount1 - fee;
+      final balance2 = 1000000000;
+      final amount2 = 8000000;
+      final changeAmount2 = balance2 - amount2 - fee;
+      final List<BcTransactionOutput> outputs = [
+        //output 1
+        BcTransactionOutput(
+            address:
+                'addr_test1qqy3df0763vfmygxjxu94h0kprwwaexe6cx5exjd92f9qfkry2djz2a8a7ry8nv00cudvfunxmtp5sxj9zcrdaq0amtqmflh6v',
+            value: BcValue(coin: amount1, multiAssets: [])),
+        BcTransactionOutput(
+            address:
+                'addr_test1qzx9hu8j4ah3auytk0mwcupd69hpc52t0cw39a65ndrah86djs784u92a3m5w475w3w35tyd6v3qumkze80j8a6h5tuqq5xe8y',
+            value: BcValue(coin: changeAmount1, multiAssets: [])),
+        //output 2
+        BcTransactionOutput(
+            address:
+                'addr_test1qrynkm9vzsl7vrufzn6y4zvl2v55x0xwc02nwg00x59qlkxtsu6q93e6mrernam0k4vmkn3melezkvgtq84d608zqhnsn48axp',
+            value: BcValue(coin: amount2, multiAssets: [])),
+        BcTransactionOutput(
+            address:
+                'addr_test1qqwpl7h3g84mhr36wpetk904p7fchx2vst0z696lxk8ujsjyruqwmlsm344gfux3nsj6njyzj3ppvrqtt36cp9xyydzqzumz82',
+            value: BcValue(coin: changeAmount2, multiAssets: [])),
+      ];
+
+      final body = BcTransactionBody(
+        inputs: inputs,
+        outputs: outputs,
+        fee: fee * 2,
+        ttl: ttl,
+        metadataHash: null,
+        validityStartInterval: 0,
+      );
+      final tx = BcTransaction(body: body, witnessSet: null, metadata: null);
+      final txHex = tx.toHex;
+      //print(txHex);
+      const expectedHex =
+          '84a4008282582073198b7ad003862b9798106b88fbccfca464b1a38afb34958275c4a7d7d8d002018258208e03a93578dc0acd523a4dd861793068a06a68b8a6c7358d0c965d2864067b68000184825839000916a5fed4589d910691b85addf608dceee4d9d60d4c9a4d2a925026c3229b212ba7ef8643cd8f7e38d6279336d61a40d228b036f40feed61a004c4b40825839008c5bf0f2af6f1ef08bb3f6ec702dd16e1c514b7e1d12f7549b47db9f4d943c7af0aaec774757d4745d1a2c8dd3220e6ec2c9df23f757a2f81a3aa5102982583900c93b6cac143fe60f8914f44a899f5329433ccec3d53721ef350a0fd8cb873402c73ad8f239f76fb559bb4e3bcff22b310b01eadd3ce205e71a007a1200825839001c1ffaf141ebbb8e3a7072bb15f50f938b994c82de2d175f358fc942441f00edfe1b8d6a84f0d19c25a9c8829442160c0b5c758094c423441a3b1b1aa3021a000b3aba031a018fb29aa0f5f6';
+      expect(txHex, expectedHex);
+      final acct1 = MultiAccountWallet.mnemonic(
+        'damp wish scrub sentence vibrant gauge tumble raven game extend winner acid side amused vote edge affair buzz hospital slogan patient drum day vital'
+            .split(' '),
+        network: NetworkId.testnet,
+      ).account();
+      print(
+          "acct_xsk: ${Bech32Coder(hrp: 'xprv').encode(acct1.basePrivateKey())}");
+      final acct2 = MultiAccountWallet.mnemonic(
+        'mixture peasant wood unhappy usage hero great elder emotion picnic talent fantasy program clean patch wheel drip disorder bullet cushion bulk infant balance address'
+            .split(' '),
+        network: NetworkId.testnet,
+      ).account();
+      final txSigned =
+          tx.sign([acct1.basePrivateKey(), acct2.basePrivateKey()]);
+      final witness1 = txSigned.witnessSet!.vkeyWitnesses[0];
+      expect(witness1.vkey, acct1.basePrivateKey().verifyKey.rawKey);
+      final expectedSig1 =
+          'bdaff70c01b89da00748579d50267a35d0d349fda3779f28e5aa99c947d41e3c9ec5b8b8dd8349278d83f099a1bcfde250c070fc9640063fba40e783e739c704';
+      expect(HEX.encode(witness1.signature), expectedSig1);
+
+      final witness2 = txSigned.witnessSet!.vkeyWitnesses[1];
+      expect(witness2.vkey, acct2.basePrivateKey().verifyKey.rawKey);
+      final expectedSig2 =
+          'd384420623677ba4e92d3b0ffe7ed7bb3037f513f75fc68d8b6462acff11314bb755a603a84f3a1a2b3b61f2661fc747b9462ffd5bc8b4641c4ec10b1e42c60a';
+      expect(HEX.encode(witness2.signature), expectedSig2);
     });
   });
 }
